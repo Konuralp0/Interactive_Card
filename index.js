eb42fa064c8a9a8349fa499b8ab0b6412bad3388
Konuralp <konuralpcelikyay@gmail.com>
2023-04-01 21:43:19 +0200

    End State Added For Completion

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+const cardHolderDiv = document.getElementById("cardholder");
+const cardNumberDiv = document.getElementById("cardnumber");
+const datecvcDiv = document.getElementById("date-cvs");
 const cardHolder = document.getElementById("cardholder-input");
 const cardOwnerP = document.getElementById("card-owner-name");
 const cardNumberP = document.getElementById("card-number");
@@ -11,8 +14,12 @@ const nameWar = document.getElementById("name-warning");
 const numWar = document.getElementById("number-warning");
 const dateWar = document.getElementById("date-warning");
 const cvcWar = document.getElementById("cvc-warning");
+const confirmButton = document.getElementById("confirm");
+
+
 
 let monthYear = 0;
+let acceptence = 0;
 
 
 cardHolder.addEventListener('input',function(){
@@ -34,6 +41,7 @@ cardHolder.addEventListener('input',function(){
     }else{
         nameWar.textContent = "";
         cardHolder.style.borderColor = 'rgba(214, 202, 202, 0.7)';
+        acceptence++;
 
     }
     
@@ -69,6 +77,7 @@ cardNumber.addEventListener('input',function(){
     }else{
         numWar.textContent = "";
         cardNumber.style.borderColor = 'rgba(214, 202, 202, 0.7)';
+        acceptence++;
 
     }
     
@@ -101,6 +110,7 @@ monthInput.addEventListener('input',function(){
         }else{
         dateWar.textContent = "";
         monthInput.style.borderColor = 'rgba(214, 202, 202, 0.7)';
+        acceptence++;
 
         }
     }else{
@@ -133,6 +143,7 @@ yearInput.addEventListener('input',function(){
         }else{
         dateWar.textContent = "";
         yearInput.style.borderColor = 'rgba(214, 202, 202, 0.7)';
+        acceptence++;
 
         }
     }else{
@@ -164,6 +175,7 @@ cvcIn.addEventListener('input',function(){
         }else{
         cvcWar.textContent = "";
         cvcIn.style.borderColor = 'rgba(214, 202, 202, 0.7)';
+        acceptence++;
 
         }
     }else{
@@ -174,3 +186,31 @@ cvcIn.addEventListener('input',function(){
 
 
 });
+
+confirmButton.addEventListener("click",function(){
+    if(acceptence >= 5 ){
+        cardHolderDiv.innerHTML = `<svg width="80" height="80" 
+        fill="none" 
+        xmlns="http://www.w3.org/2000/svg">
+        <circle cx="40" cy="40" r="40" 
+        fill="url(#a)"/><path 
+        d="M28 39.92 36.08 48l16-16" 
+        stroke="#fff" stroke-width="3"/>
+        <defs><linearGradient id="a" x1="-23.014" 
+        y1="11.507" x2="0" y2="91.507" 
+        gradientUnits="userSpaceOnUse"
+        ><stop stop-color="#6348FE"/>
+        <stop offset="1" stop-color="#610595"/></linearGradient></defs></svg>`;
+        cardNumberDiv.innerHTML = `<h1 style="color: rgb(83, 58, 175); font-family:'Space Grotesk', sans-serif;font-weight: 600;"> Thank You!</h1>`;
+        datecvcDiv.innerHTML = `<h3 style = "color: rgb(134, 123, 173);font-family:'Space Grotesk', sans-serif;">We've added yourd card details</h3>`;
+        confirmButton.textContent = "Continue";
+        
+        document.getElementById("indiv-right").style.alignItems = "center";
+    }
+
+
+
+
+
+
+})
